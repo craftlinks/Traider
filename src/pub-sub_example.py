e11e7e14ed0660c3f6648ec37f0ba69a79955c6a
craftlinks <craftlinks@protonmail.com>
2025-08-30 23:25:53 +0200

    planning for the messagebus package

--- src/pub-sub_example.py
+++ src/pub-sub_example.py
@@ -8,51 +8,67 @@ import os
 import pprint
 import signal
 import time
-from typing import Awaitable, Callable, Protocol, TypeVar
+from typing import Any, Awaitable, Callable, Literal, Protocol, TypeVar, overload
 
-from traider.yfinance import EarningsEvent
+from traider.yfinance import EarningsEvent, PressRelease
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 MAX_EVENTS = 5
 
 class Channel(str, Enum):
     EARNINGS = "earnings"
+    PRESS_RELEASE = "press_release"
 
 
-Message = TypeVar("Message")
-
-
-class MessageBroker(Protocol[Message]):
-    async def publish(self, channel_name: Channel, message: Message) -> None:
-        ...
-
-    async def subscribe(self, channel_name: Channel) -> asyncio.Queue[Message]:
-        ...
-
-    def unsubscribe(self, channel_name: str, queue: asyncio.Queue[Message]) -> None:
-        ...
-
-
-class InMemoryBroker(MessageBroker[Message]):
-    def __init__(self):
-        self.channels = defaultdict(list[asyncio.Queue[Message]])
+class MessageBroker(Protocol):
     
-    async def publish(self, channel_name: Channel, message: Message) -> None:
+    @overload
+    async def publish(self, channel_name: Literal[Channel.EARNINGS], message: EarningsEvent) -> None:
+        ...
+
+    @overload
+    async def publish(self, channel_name: Literal[Channel.PRESS_RELEASE], message: PressRelease) -> None:
+        ...
+
+
+    @overload
+    async def subscribe(self, channel_name: Literal[Channel.EARNINGS]) -> asyncio.Queue[EarningsEvent]:
+        ...
+
+    @overload
+    async def subscribe(self, channel_name: Literal[Channel.PRESS_RELEASE]) -> asyncio.Queue[PressRelease]:
+        ...
+
+    async def publish(self, channel_name: Channel, message: Any) -> None:
+        ...
+
+    async def subscribe(self, channel_name: Channel) -> asyncio.Queue[Any]:
+        ...
+
+    def unsubscribe(self, channel_name: Channel, queue: asyncio.Queue[Any]) -> None:
+        ...
+
+
+class InMemoryBroker(MessageBroker):
+    def __init__(self):
+        self.channels = defaultdict(list[asyncio.Queue[Any]])
+    
+    async def publish(self, channel_name: Channel, message: Any) -> None:
         for queue in self.channels[channel_name]:
             await queue.put(message)
         
-    async def subscribe(self, channel_name: Channel) -> asyncio.Queue[Message]:
+    async def subscribe(self, channel_name: Channel) -> asyncio.Queue[Any]:
        queue = asyncio.Queue()
        self.channels[channel_name].append(queue)
        return queue
     
-    def unsubscribe(self, channel_name: str, queue: asyncio.Queue[Message]) -> None:
+    def unsubscribe(self, channel_name: Channel, queue: asyncio.Queue[Any]) -> None:
         if queue in self.channels[channel_name]:
             self.channels[channel_name].remove(queue)
 
-async def worker_loop(worker_name: str, queue: asyncio.Queue[Message], shutdown_event: asyncio.Event, worker_fn: Callable[[Message], Awaitable[None]]):
+async def worker_loop(worker_name: str, queue: asyncio.Queue[Any], shutdown_event: asyncio.Event, worker_fn: Callable[[Any], Awaitable[None]]):
     while True:
         try:
             message = await asyncio.wait_for(queue.get(), timeout=1)
@@ -74,7 +90,7 @@ def _cpu_bound_worker_fn(message: EarningsEvent) -> bool:
 
 async def cpu_bound_worker_1(
     process_pool: ProcessPoolExecutor,
-    broker: MessageBroker[EarningsEvent],
+    broker: MessageBroker,
     shutdown_event: asyncio.Event,
     startup_barrier: asyncio.Barrier,
 ):
@@ -85,7 +101,7 @@ async def cpu_bound_worker_1(
 
     loop = asyncio.get_running_loop()
 
-    async def process_event(event: EarningsEvent):
+    async def process_event(event: Any):
         should_unsubscribe = await loop.run_in_executor(process_pool, _cpu_bound_worker_fn, event)
         # Check if shutdown was triggered while we were blocked in the executor
         if shutdown_event.is_set():
@@ -98,7 +114,7 @@ async def cpu_bound_worker_1(
     await worker_loop("cpu_bound_worker_1", queue, shutdown_event, process_event)
 
 
-async def test_earnings_producer(mbr: MessageBroker[EarningsEvent], shutdown_event: asyncio.Event, startup_barrier: asyncio.Barrier):
+async def earnings_producer(msg_broker: MessageBroker, shutdown_event: asyncio.Event, startup_barrier: asyncio.Barrier):
     
     await startup_barrier.wait()
     
@@ -124,7 +140,7 @@ async def test_earnings_producer(mbr: MessageBroker[EarningsEvent], shutdown_eve
             market_cap=1.0,
         )
 
-        await mbr.publish(Channel.EARNINGS, earnings_event)
+        await msg_broker.publish(Channel.EARNINGS, earnings_event)
 
         earnings_event_id += 1
         await asyncio.sleep(1)
@@ -132,26 +148,32 @@ async def test_earnings_producer(mbr: MessageBroker[EarningsEvent], shutdown_eve
     logging.debug("Producer shutting down...")
 
 
-async def test_earnings_worker(mbr: MessageBroker[EarningsEvent], shutdown_event: asyncio.Event, startup_barrier: asyncio.Barrier):
+async def earnings_press_release_poller(company_name: str, ticker: str):
+    pass
+
+
+async def start_earnings_press_release_poller(msg_broker: MessageBroker, shutdown_event: asyncio.Event, startup_barrier: asyncio.Barrier):
     
-    queue = await mbr.subscribe(Channel.EARNINGS)
+    queue = await msg_broker.subscribe(Channel.EARNINGS)
 
     await startup_barrier.wait()
     
     async def worker_fn(message: EarningsEvent):
-        pprint.pprint(f"worker 1: Received earnings event: {message}")
+        pprint.pprint(f"worker 1: Received earnings event for {message.company_name}")
+        # start long running process to poll press release
+        asyncio.create_task(earnings_press_release_poller(message.company_name, message.ticker))
         if message.id > MAX_EVENTS:
-            mbr.unsubscribe(Channel.EARNINGS, queue)
+            msg_broker.unsubscribe(Channel.EARNINGS, queue)
             shutdown_event.set()
             return
         await asyncio.sleep(1)
     
-    await worker_loop("test_earnings_worker_1", queue, shutdown_event, worker_fn)
+    await worker_loop("press_release_worker_1", queue, shutdown_event, worker_fn)
 
 
 async def main():
     
-    mbr = InMemoryBroker()
+    msg_broker = InMemoryBroker()
     shutdown_event = asyncio.Event()
     startup_barrier = asyncio.Barrier(parties=3)
     cpu_cores = os.cpu_count() or 1
@@ -164,9 +186,9 @@ async def main():
     
     with ProcessPoolExecutor(max_workers=cpu_cores) as process_pool:
         await asyncio.gather(
-            test_earnings_producer(mbr, shutdown_event, startup_barrier),
-            test_earnings_worker(mbr, shutdown_event, startup_barrier),
-            cpu_bound_worker_1(process_pool, mbr, shutdown_event, startup_barrier)
+            earnings_producer(msg_broker, shutdown_event, startup_barrier),
+            press_release_producer(msg_broker, shutdown_event, startup_barrier),
+            cpu_bound_worker_1(process_pool, msg_broker, shutdown_event, startup_barrier)
         )
     
 
